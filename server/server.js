75571c0067fac0641b019d7d6ffc46fc6285e244
Manas Choudhary <mannchoudhary3431@gmail.com>
2024-05-02 16:10:10 +0530

    server.js added

---
+++ server/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router");
+const connectDB = require("./utils/db");
+const errorMiddleware = require("./middlewares/error-middleware");
+
+//middlewares
+//middleware for using json objects
+app.use(express.json());
+
+//router
+app.use("/api/auth", authRoute);
+app.use("/api/Form", contactRoute);
+//useless
+// app.get("/", function (req, res) {
+//   res.status(200).send("welcome");
+// });
+// app.get("/register", function (req, res) {
+//   res.status(200).send("YOOOOOO");
+// });
+
+//middleware for error handling which is to be written before connection only
+app.use(errorMiddleware);
+
+const PORT = 5000;
+
+//connect to databse first and then start the servet=
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is running at port: ${PORT}`);
+  });
+});
