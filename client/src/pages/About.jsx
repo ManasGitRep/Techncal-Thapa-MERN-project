32bfb7667f24e0557dbb263282c3cdbeb7ecb10e
Manas Choudhary <mannchoudhary3431@gmail.com>
2024-06-27 22:02:51 +0530

    created about page for the app

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,44 @@
 export const About = () => {
-  return <h1>About page</h1>;
+  return (
+    <section className="section-hero">
+      <div className="container grid grid-two-cols">
+        {/* hero-image */}
+        <div className="hero-image">
+          <img
+            src="/images/login.jpg"
+            alt="coding together"
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className="hero-content">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+          <h1>Lorem ipsum dolor sit amet.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem alias
+            labore quas pariatur distinctio nisi repellendus dolor vel doloribus
+            porro?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem alias
+            labore quas pariatur distinctio nisi repellendus dolor vel doloribus
+            porro? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut,
+            laborum!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem alias
+            labore quas pariatur distinctio nisi repellendus
+          </p>
+          <div className="btn btn-group">
+            <a href="/contact">
+              <button className="btn">connect now</button>
+            </a>
+            <a href="/service">
+              <button className="btn secondary-btn">learn more</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
