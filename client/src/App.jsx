3d397a489db909f8e3bf3b8ef8237754edbbc165
Manas Choudhary <mannchoudhary3431@gmail.com>
2024-07-24 17:51:33 +0530

    done with login page and error page with styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
+import { Error } from "./pages/Error";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
           <Route path="/service" element={<Service />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Error />} />
         </Routes>
         <Footer />
       </BrowserRouter>
